1ecca07251d9f2bfef157d2741e4b6c9bbbe105d
Shishkin Sergey <shishkinsergeydmitrievich@gmail.com>
2024-10-17 23:38:24 +0300

    feat: now completion don't show redundant options

--- gen-release-notes-completion.sh
+++ gen-release-notes-completion.sh
@@ -1,2 +1,39 @@
 #!/usr/bin/env bash
-complete -W "--help --version --raw-titles --short --all-commits --single-list --from-latest-tag --format" gen-release-notes
+
+function _gen_release_notes_completion()
+{
+  latest="${COMP_WORDS[$COMP_CWORD]}"
+  prev="${COMP_WORDS[$COMP_CWORD - 1]}"
+  # if there are no commands yet search for commands
+  words="-h --help -v --version --show-repo-config
+         --raw-titles -f -a --all-commits --single-list -lt --from-latest-tag --short --format"
+
+  case "$prev" in
+  -f)
+    words=""  # TODO use directory completion
+    ;;
+  --format)
+    words=""
+    ;;
+  esac
+
+  # remove completion if standalone options was typed
+  for i in "${COMP_WORDS[@]}"; do
+    case "$i" in
+    -h|--help)
+      words=""
+      ;;
+    -v|--version)
+      words=""
+      ;;
+    --show-repo-config)
+      words=""
+      ;;
+    esac
+  done
+
+  # shellcheck disable=SC2207
+  COMPREPLY=( $(compgen -W "$words" -- "$latest") )
+}
+
+complete -F _gen_release_notes_completion gen-release-notes
